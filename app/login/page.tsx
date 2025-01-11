14babf504221ced2a10d0f1f09ca7872239592b0
Zafer Fatih Koyuncu <zaferfatih@gmail.com>
2025-01-11 23:21:26 +0300

    login page styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,16 +19,15 @@ export default function LoginPage() {
         );
       };
     
-      return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', flexDirection: 'column' }}>
-            <h1 style={{ color: 'purple', fontFamily: 'Arial, sans-serif', fontSize: '2rem' }}>Welcome to Kids Math Puzzles</h1>
-            <p style={{ color: 'purple', fontFamily: 'Arial, sans-serif', fontSize: '1.2rem' }}>Please log in to continue</p>
-            <div style={{ border: '2px solid #ccc', padding: '20px', borderRadius: '10px', marginTop: '20px' }}>
+    return (
+      <div className="container max-sm:mx-auto px-4 py-8">
+        
+        <p className="text-xl font-bold text-center text-purple-700 mb-8">{translate('login.introText')}</p>
+        <div style={{ display: "flex", justifyContent: "center" }}>
             <GoogleOAuthProvider clientId="<YOUR_CLIENT_ID>">
-            <GoogleLoginButton />
+              <GoogleLoginButton />
             </GoogleOAuthProvider>
-            </div>
         </div>
-      );
-
+      </div>
+    );
 };
